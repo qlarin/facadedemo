c65a5d11bc672ee9ebf8bba8fd5d167af4528766
s11617 <st.sledziona@gmail.com>
2015-04-06 15:58:17 +0200

    first commit

---
+++ FacadeDemo/src/FacadeTest.java
@@ -0,0 +1,11 @@
+
+public class FacadeTest {
+
+	public static void main(String[] args){
+		ShapeMaker shapeMaker = new ShapeMaker();
+		
+		shapeMaker.drawRectangle();
+		shapeMaker.drawSquare();
+		shapeMaker.drawCircle();
+	}
+}
